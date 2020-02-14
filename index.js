ae980188a5ff816b213292e9e9380dc5bbd7a2c3
seattleowl <seattleowl@users.noreply.github.com>
2020-02-13 17:38:14 -0800

    Add socket.io

--- index.js
+++ index.js
@@ -6,12 +6,19 @@
 */
 
 const express = require("express")
+const socket = require("socket.io")
 const app = express()
+const http = require("http").createServer(app)
+const io = socket(http)
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/src/html/index.html")
 })
 
+io.on("connection", () => {
+    console.log("Socket working")
+})
+
 app.use("/src", express.static("src"))
 
-app.listen(process.env.PORT)
+http.listen(process.env.PORT)
